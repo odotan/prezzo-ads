1aa909cb2917c6cdc3673aa3e07419e9cd1d502b
Itay Pincas <itaypincas@icloud.com>
2016-11-21 20:01:42 +0200

    Main Screen nabber icon.

---
+++ PRZMainAdvertisersTVC.h
@@ -0,0 +1,13 @@
+//
+//  PRZMainAdvertisersTVC.h
+//  PREZZO
+//
+//  Created by Itay Pincas on 11/21/16.
+//  Copyright © 2016 Itay Pincas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PRZMainAdvertisersTVC : UITableViewController
+
+@end
